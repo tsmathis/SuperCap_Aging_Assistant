4d62c0cee927ece9e9955c571b563db4598f0004
Tyler Mathis <tmathis204@gmail.com>
2022-09-20 23:09:53 +0200

    pyexcelerate

--- data_export.py
+++ data_export.py
@@ -1,5 +1,17 @@
 from pyexcelerate import Workbook
 
 
-def export_data(destination, aging_data, cvs, eis):
-    pass
+def export_data(
+    destination,
+    aging_data=None,
+    cvs_before=None,
+    cvs_after=None,
+    eis_before=None,
+    eis_after=None,
+):
+
+    aging_wb = Workbook()
+    cvs_before_wb = Workbook()
+    cvs_after_wb = Workbook()
+    eis_before_wb = Workbook()
+    eis_after_wb = Workbook()
